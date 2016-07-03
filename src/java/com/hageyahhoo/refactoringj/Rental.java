0ebe4fb95428964e960129f500cd1aa57b8bda7d
Hiroyuki Ito <hageyahhoo@gmail.com>
2016-07-03 20:37:40 +0900

    Adopted Move Method and Replace Temp with Query.

--- src/java/com/hageyahhoo/refactoringj/Rental.java
+++ src/java/com/hageyahhoo/refactoringj/Rental.java
@@ -10,12 +10,39 @@ public class Rental {
         _daysRented = daysRented;
     }
 
-
     public Movie getMovie() {
         return _movie;
     }
 
     public int getDaysRented() {
         return _daysRented;
+    }
+
+
+    // Extract Method & Introduce Explaining Variable & Move Method
+    public double getCharge() {
+        double result = 0;
+
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (this.getDaysRented() > 2) {
+                    result += (this.getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie.NEW_RELEASE:
+                result += this.getDaysRented() * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (this.getDaysRented() > 3) {
+                    result += (this.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+
+        return result;
     }
 }
